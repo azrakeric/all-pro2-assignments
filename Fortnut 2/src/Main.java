7e611260e12c7b92c636212e6cf95242d9a5ac4c
Unknown <mark@heijnekamp.nl>
2018-12-29 02:28:03 +0100

    ideas
    stated my start @ fornut 2 
    my thoughts and risks i have

---
+++ Fortnut 2/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //een speler heeft levens attack defense max weight
+        //elk wapen heeft stregth durabilty weight accuracy
+        // pantser en schild heeft defense durablity en weight
+       // classes
+        // - Player
+        //- Weapon
+        //- Shield
+        //- Armour
+        //- Game
+        //- Menu
+
+
+        // gedachte nu een grid / map  van 16 bij 16
+        // maak arrays voor een grid
+        // als 2 spelers op 1 vakje staan in het grid kunnen ze vechten
+        // als er een huisje is kunnen ze loot pakken
+        // alles is random
+        //dus plaatsing huisjes en loot
+
+        //risicos
+        //in de beschrijving staat dat je wapens moet kiezen aan het begin van het spel
+    }
+}
